7013cdd59ff6f9381f2af5ad76da34922c9a94d9
KDS-23 <kds.kukurudza@gmail.com>
2020-08-27 17:39:29 +0300

    ver. 1.0

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() { 
+  // -------------------------------- NAV WITH BURGER -------------------
+  $('.menu__button--burger').click(function(e){
+    $('.menu').slideToggle(700);
+}) 
+  $(".header__button--burger").click(function() {      
+      $(".header__button--burger").toggleClass("active");
+      $(".menu").toggleClass("active");
+  });  
+});
+// -------------------------------- SLIDER ----------------------------
+$(document).ready(function(){
+  $('.slider__main').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    prevArrow: $('.slider__button--previous'),
+    nextArrow: $('.slider__button--next'),
+  })
+})
